12a66135463ae1798030273d7ec93c8441123723
Ashwini <ashie.oh@gmail.com>
2017-04-03 21:50:07 +0100

    Add a feature test editing

--- spec/features/resturants_feature_spec.rb
+++ spec/features/resturants_feature_spec.rb
@@ -47,4 +47,21 @@ feature 'resturants' do
     end
   end
 
+  context 'editing restaurants' do
+
+  before { Resturant.create name: 'KFC', description: 'Deep fried goodness', id: 1 }
+  scenario 'let a user edit a restaurant' do
+    visit '/resturants'
+    click_link 'Edit KFC'
+    fill_in 'Name', with: 'Kentucky Fried Chicken'
+    fill_in 'Description', with: 'Deep fried goodness'
+    click_button 'Update Resturant'
+    click_link 'Kentucky Fried Chicken'
+    expect(page).to have_content 'Kentucky Fried Chicken'
+    expect(page).to have_content 'Deep fried goodness'
+    expect(current_path).to eq '/resturants/1'
+  end
+
+end
+
 end
