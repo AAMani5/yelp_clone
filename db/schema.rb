c94366996b3158a2ea191572a7f4a767aca0f9a6
Ashwini <ashie.oh@gmail.com>
2017-04-04 13:07:45 +0100

    Add new schema after db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170404105938) do
+ActiveRecord::Schema.define(version: 20170404114928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,9 @@ ActiveRecord::Schema.define(version: 20170404105938) do
     t.integer  "rating"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "resturant_id"
   end
+
+  add_index "reviews", ["resturant_id"], name: "index_reviews_on_resturant_id", using: :btree
 
 end
