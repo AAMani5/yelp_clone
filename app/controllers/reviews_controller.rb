661787a43393abd714a8a1d0f6a3504ba0e8790b
Ashwini <ashie.oh@gmail.com>
2017-04-04 13:09:06 +0100

    Amend #create

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,4 +4,16 @@ class ReviewsController < ApplicationController
     @resturant = Resturant.find(params[:resturant_id])
     @review = Review.new
   end
+
+  def create
+    @resturant = Resturant.find(params[:resturant_id])
+    @resturant.reviews.create(review_params)
+    redirect_to '/resturants'
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:thoughts, :rating)
+  end
 end
