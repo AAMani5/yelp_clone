3db489bf6d9d7bad496f7fb73454f2608bfb957e
Ashwini <ashie.oh@gmail.com>
2017-04-06 21:13:00 +0100

    Migration done. reviews has user_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170406200118) do
+ActiveRecord::Schema.define(version: 20170406200856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,9 +33,11 @@ ActiveRecord::Schema.define(version: 20170406200118) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "resturant_id"
+    t.integer  "user_id"
   end
 
   add_index "reviews", ["resturant_id"], name: "index_reviews_on_resturant_id", using: :btree
+  add_index "reviews", ["user_id"], name: "index_reviews_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -58,4 +60,5 @@ ActiveRecord::Schema.define(version: 20170406200118) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "resturants", "users"
+  add_foreign_key "reviews", "users"
 end
