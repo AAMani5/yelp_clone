e1944cb3ca278e7871e086cb89bd4c1a28d7069c
Ashwini <ashie.oh@gmail.com>
2017-04-06 15:29:21 +0100

    Add test to check if only logged in user can create resturants

--- spec/features/resturants_feature_spec.rb
+++ spec/features/resturants_feature_spec.rb
@@ -2,9 +2,50 @@ require 'rails_helper'
 
 feature 'resturants' do
 
-  context 'no resturants have been added' do
-    scenario 'should display a prompt to add a resturant' do
+  context 'can' do
+    before do
+      Resturant.create name: 'KFC', description: 'Deep fried goodness'
+      visit('/')
+      click_link('Sign up')
+      fill_in('Email', with: 'test@example.com')
+      fill_in('Password', with: 'testtest')
+      fill_in('Password confirmation', with: 'testtest')
+      click_button('Sign up')
+      click_link('Sign out')
+    end
+
+    scenario ' not be created if a user is not logged in' do
       visit '/resturants'
+      expect(page).to have_content 'KFC'
+      expect(page).not_to have_link 'Add a resturant'
+    end
+
+    scenario ' be created by logged in users' do
+      visit('/users/sign_in')
+      fill_in('Email', with: 'test@example.com')
+      fill_in('Password', with: 'testtest')
+      click_button('Log in')
+      expect(page).to have_content('Signed in successfully.')
+      expect(page).to have_link 'Add a resturant'
+    end
+  end
+
+  context 'no resturants have been added' do
+    before do
+      visit('/')
+      click_link('Sign up')
+      fill_in('Email', with: 'test@example.com')
+      fill_in('Password', with: 'testtest')
+      fill_in('Password confirmation', with: 'testtest')
+      click_button('Sign up')
+      click_link('Sign out')
+    end
+
+    scenario 'should display a prompt to add a resturant' do
+      visit('/users/sign_in')
+      fill_in('Email', with: 'test@example.com')
+      fill_in('Password', with: 'testtest')
+      click_button('Log in')
       expect(page).to have_content 'No resturants yet'
       expect(page).to have_link 'Add a resturant'
     end
@@ -25,14 +66,33 @@ end
 
 context 'creating resturants' do
 
+  before do
+    visit('/')
+    click_link('Sign up')
+    fill_in('Email', with: 'test@example.com')
+    fill_in('Password', with: 'testtest')
+    fill_in('Password confirmation', with: 'testtest')
+    click_button('Sign up')
+  end
+
   scenario 'prompts user to fill out a form, then displays the new resturant' do
-    visit '/resturants'
     click_link 'Add a resturant'
-    fill_in 'Name', with: 'KFC'
+    expect(current_path).to eq '/resturants/new'
+    fill_in 'resturant[name]' , with: 'KFC'
     click_button 'Create Resturant'
     expect(page).to have_content 'KFC'
     expect(current_path).to eq '/resturants'
   end
+
+  scenario 'does not let you submit a name that is too short' do
+    visit '/resturants'
+    click_link 'Add a resturant'
+    fill_in 'Name', with: 'kf'
+    click_button 'Create Resturant'
+    expect(page).not_to have_css 'h2', text: 'kf'
+    expect(page).to have_content 'error'
+  end
+
 end
 
 context 'viewing resturants' do
@@ -49,7 +109,16 @@ end
 
 context 'editing resturants' do
 
-  before { Resturant.create name: 'KFC', description: 'Deep fried goodness', id: 1 }
+  before  do
+    Resturant.create name: 'KFC', description: 'Deep fried goodness', id: 1
+    visit('/')
+    click_link('Sign up')
+    fill_in('Email', with: 'test@example.com')
+    fill_in('Password', with: 'testtest')
+    fill_in('Password confirmation', with: 'testtest')
+    click_button('Sign up')
+  end
+
   scenario 'let a user edit a restaurant' do
     visit '/resturants'
     click_link 'Edit KFC'
@@ -66,7 +135,15 @@ end
 
 context 'deleting resturants' do
 
-  before { Resturant.create name: 'KFC', description: 'Deep fried goodness' }
+  before  do
+    Resturant.create name: 'KFC', description: 'Deep fried goodness', id: 1
+    visit('/')
+    click_link('Sign up')
+    fill_in('Email', with: 'test@example.com')
+    fill_in('Password', with: 'testtest')
+    fill_in('Password confirmation', with: 'testtest')
+    click_button('Sign up')
+  end
 
   scenario 'removes a resturant when a user clicks a delete link' do
     visit '/resturants'
@@ -76,18 +153,5 @@ context 'deleting resturants' do
   end
 
 end
-
-context 'creating resturants' do
-
-  scenario 'does not let you submit a name that is too short' do
-    visit '/resturants'
-    click_link 'Add a resturant'
-    fill_in 'Name', with: 'kf'
-    click_button 'Create Resturant'
-    expect(page).not_to have_css 'h2', text: 'kf'
-    expect(page).to have_content 'error'
-  end
-end
-
 
 end
