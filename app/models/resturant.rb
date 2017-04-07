28eb99bea8f5f38803c07b8bead06197b55cfb50
Bernard Malhame <bernard.malhame@gmail.com>
2017-04-07 14:51:33 +0100

    extending active record associations

--- app/models/resturant.rb
+++ app/models/resturant.rb
@@ -1,5 +1,7 @@
 class Resturant < ActiveRecord::Base
   validates :name, length: {minimum: 3}, uniqueness: true
-  has_many :reviews, dependent: :destroy
+  has_many :reviews,
+ -> { extending WithUserAssociationExtension },
+  dependent: :destroy
   belongs_to :user
 end
