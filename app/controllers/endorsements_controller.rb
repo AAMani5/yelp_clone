e2490f8da21ba553e3f0d6a4c2eaac46238747a9
Ashwini <ashie.oh@gmail.com>
2017-04-08 08:26:45 +0100

    Return JSON instead of redirect to resturants#index, #method removed as review_endorsements_path led to post endorsements#create

--- app/controllers/endorsements_controller.rb
+++ app/controllers/endorsements_controller.rb
@@ -1,15 +1,9 @@
 class EndorsementsController < ApplicationController
 
-  def new
-    # @review = Review.find(params[:review_id])
-    # @endorsement = Endorsement.new
-    create
-  end
-
   def create
      @review = Review.find(params[:review_id])
      @review.endorsements.create
-     redirect_to resturants_path
+     render json: {new_endorsement_count: @review.endorsements.count}
    end
 
 end
