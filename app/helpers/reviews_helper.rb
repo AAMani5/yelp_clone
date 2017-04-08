00973ea878c9d4cfc9b9fe372e3f5317afd528e2
Ashwini <ashie.oh@gmail.com>
2017-04-08 12:45:18 +0100

    Used a helper for star_rating method

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,7 @@
 module ReviewsHelper
+  def star_rating(rating)
+    return 'No Ratings, Be the first to rate' unless rating.respond_to?(:round)
+    remainder = 5 - rating.round
+    "★" * rating.round + "☆" * remainder
+  end
 end
